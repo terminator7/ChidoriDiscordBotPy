a248e64adc43b3fda8fdbdb3cd6dc931672f07c0
terminator7 <9858294+terminator7@users.noreply.github.com>
2025-06-01 13:20:33 -0500

    testing out some more commands

--- ChidoriDiscordClient.py
+++ ChidoriDiscordClient.py
@@ -1,31 +1,35 @@
 import discord
-from typing import Callable, Any
+from typing import Callable, Any, Awaitable
 from enum import Enum
 
 
 class ChidoriDiscordBot(discord.Client):
-    def __init__(self, **options) -> None:
+    def __init__(self, **options,) -> None:
         super().__init__(**options)
-        self.command_symbol = "!"
-        self.command_list = {}
+        self.__command_symbol = "!"
+        self.__command_list = {}
+        self.__server_id = 0
+        self.server = self.get_guild(0)
 
-    def add_command(self, command : str, commandCallback : Callable[[discord.Message], None]) -> None:
-        self.command_list[command] = commandCallback
+    def add_command(self, command : str, commandCallback : Callable[["ChidoriDiscordBot", discord.Message], Awaitable[None]]) -> None:
+        self.__command_list[command] = commandCallback
 
-
+    def add_server_id(self, server_id : int) -> None:
+        self.__server_id = server_id
 
     async def setup_hook(self):
         pass
 
     async def on_ready(self) -> None:
+        self.server = self.get_guild(self.__server_id)
         print(f"Logged in as {self.user}")
 
     async def on_message(self, message: discord.Message) -> None:
-       if message.content[0] == self.command_symbol:
+       if message.content[0] == self.__command_symbol:
         message_array = message.content[1:].split(" ")
-        if message_array[0] in self.command_list.keys():
-            command_callable = self.command_list[message_array[0]]
-            command_callable(message)
+        if message_array[0] in self.__command_list.keys():
+            command_callable = self.__command_list[message_array[0]]
+            await command_callable(self, message)
         else:    
             print("error command was not found")
            
--- main.py
+++ main.py
@@ -9,11 +9,38 @@ load_dotenv()
 # --- Discord Information ---
 DISCORD_TOKEN: str = str(os.getenv("SECRET_KEY"))
 
-def testing_command_handler(discord_message: discord.Message) -> None:
+async def get_discord_ids(discord_client: ChidoriDiscordBot, discord_message: discord.Message) -> None:
+    args = (discord_message.content.split(" "))[1:]
+    server = discord_message.guild
+    if server == None:
+        print(f"Command in question: {discord_message.content.split(" ")[0]}\nError: Could not find server ID from some reason")
+    else:
+        members_message = ""
+        async for member in server.fetch_members():
+            members_message += f"{member.display_name}: {member.id}\n"
+        channel = discord_message.channel
+        await channel.send(members_message)
+
+
+async def testing_command_handler(discord_client: ChidoriDiscordBot, discord_message: discord.Message) -> None:
     args = (discord_message.content.split(" "))[1:]
     print(args)
     channel_id = discord_message.channel
-    asyncio.create_task(channel_id.send(f"{args}"))
+    await channel_id.send(f"{args}")
+
+async def send_dm_to_person(discord_client: ChidoriDiscordBot, discord_message: discord.Message) -> None:
+    args = (discord_message.content.split(" "))[1:]
+    print(args)
+    discord_user = discord_client.get_user(int(args[0]))
+    message = "".join(f"{word} " for word in args[1:])
+    if discord_user != None:
+        try:
+            await discord_user.send(message)
+        except Exception as e:
+            print(e)
+    else:
+        print("eat shit")
+    
 
 
 def main():
@@ -22,8 +49,14 @@ def main():
 
     discord_client = ChidoriDiscordBot(intents=discord.Intents.all())
 
-    discord_client.add_command("test", testing_command_handler)
+    # -- Initialize --
+    guild_id = str(os.getenv("GUILD_ID"))
+    discord_client.add_server_id(int(guild_id))
 
+    # -- Commands --
+    discord_client.add_command("test", testing_command_handler)
+    discord_client.add_command("members", get_discord_ids)
+    discord_client.add_command("message", send_dm_to_person)
     discord_client.run(DISCORD_TOKEN)
 
 
